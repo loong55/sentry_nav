38ec8139c8a684c73aa29caf7073e573c2bd4de3
loong55 <1515415975@qq.com>
2025-12-20 11:08:37 +0800

    更新地图保存序号和行为树配置，启用三点循环

--- save_map.sh
+++ save_map.sh
@@ -3,4 +3,4 @@
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-ros2 run nav2_map_server map_saver_cli -f 1
+ros2 run nav2_map_server map_saver_cli -f 2
--- start_all.sh
+++ start_all.sh
@@ -3,8 +3,8 @@
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 
-ros2 launch pb2025_nav_bringup rm_navigation_reality_launch.py world:=1 slam:=False use_robot_state_pub:=True &
-# ros2 launch pb2025_sentry_behavior pb2025_sentry_behavior_launch.py &
+ros2 launch pb2025_nav_bringup rm_navigation_reality_launch.py world:=2 slam:=False use_robot_state_pub:=True &
+ros2 launch pb2025_sentry_behavior pb2025_sentry_behavior_launch.py &
 
 sleep 5
 
