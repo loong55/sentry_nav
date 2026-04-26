4c7c4f24bec2165865777cffb05005767a2b192f
loong55 <1515415975@qq.com>
2026-04-26 08:58:31 +0800

    更新仿真配置，调整重力参数和机器人位置，优化仿真环境

--- gazebo.sh
+++ gazebo.sh
@@ -1,4 +1,5 @@
 source /opt/ros/humble/setup.bash
 source install/setup.bash
-# ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2026
-ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2025
+ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2026
+# ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2025
+# ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2024
