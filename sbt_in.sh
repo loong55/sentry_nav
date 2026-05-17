1c8d05c6a7d431437a63561259e4b46962c0096b
loong55 <1515415975@qq.com>
2026-05-18 04:16:15 +0800

    rmun_in行为树优化

---
+++ sbt_in.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+export ROS_DOMAIN_ID=5  # 宿主机在 5 号网段
+
+# 指定位姿参数文件示例：
+#     pose:=pose.yaml
+# 指定行为树名称示例：
+#     target_tree:=2026rmuc
+
+ros2 launch pb2025_sentry_behavior pb2025_sentry_behavior_launch.py \
+    pose:=pose_in.yaml \
+    target_tree:=2026rmuc_in \
+    use_sim_time:=True \
+    namespace:=red_standard_robot1
+
+# ros2 launch pb2025_sentry_behavior pb2025_sentry_behavior_launch.py \
+#     pose:=pose_test.yaml \
+#     target_tree:=2026rmuc \
+#     use_sim_time:=True \
+#     namespace:=red_standard_robot1    
