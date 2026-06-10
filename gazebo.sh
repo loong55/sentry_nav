9b22d73489cf0221adfaae105c03d59da919d5d7
loong55 <1515415975@qq.com>
2026-06-10 17:12:20 +0800

    文档优化

--- gazebo.sh
+++ gazebo.sh
@@ -2,8 +2,8 @@ source /opt/ros/humble/setup.bash
 source install/setup.bash
 export ROS_DOMAIN_ID=5  # 宿主机在 5 号网段
 
-ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=gazebo_test_field
-# ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2026fix
+# ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=gazebo_test_field
+ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2026fix
 # ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2026
 # ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2025
 # ros2 launch rmu_gazebo_simulator bringup_sim.launch.py world:=rmuc_2024
